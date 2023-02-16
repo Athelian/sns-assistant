7b1746716bfd6c4726a78d2ef5bb49c5199c1f4a
Eliot Austin-Forbes <ejaustinforbes@gmail.com>
2023-02-16 19:04:38 +0900

    refactor: for later translations

--- app/page.tsx
+++ app/page.tsx
@@ -25,29 +25,29 @@ export default function Home() {
         <div className={styles.slogan}>Install a new marketing team.</div>
       </header>
       <section className={styles.body}>
-        <Section
-          variant="dark"
-          header={
-            'Elevate your social media presence with artificial intelligence'
-          }
-          body={
-            'Integrate world class machine learning with your business platform to manage your social edia presence. By using SNAI, you are leading the way in the new world of automated marketing.'
-          }
-        />
-        <Section
-          variant="light"
-          header={'Turbocharge your engagement with online audiences'}
-          body={
-            'Respond to enquiries the moment they arrive with your latest product information and in a style that represents your brand.'
-          }
-        />
-        <Section
-          variant="dark"
-          header={"Automate your brand's advertising"}
-          body={
-            'With Snai you can generate new product showcases without lifting a finger. Integrate your product line and let your new team take care of the rest.'
-          }
-        />
+        {[
+          [
+            'Elevate your social media presence with artificial intelligence',
+            'Integrate world class machine learning with your business platform to manage your social edia presence. By using SNAI, you are leading the way in the new world of automated marketing.',
+          ],
+          [
+            'Turbocharge your engagement with online audiences',
+            'Respond to enquiries the moment they arrive with your latest product information and in a style that represents your brand.',
+          ],
+          [
+            "Automate your brand's advertising",
+            'With Snai you can generate new product showcases without lifting a finger. Integrate your product line and let your new team take care of the rest.',
+          ],
+        ].map(([header, body], i) => {
+          return (
+            <Section
+              key={i}
+              variant={i % 2 === 0 ? 'dark' : 'light'}
+              header={header}
+              body={body}
+            />
+          )
+        })}
       </section>
       <footer className={styles.footer}>
         <span>Created by Eliot Austin-Forbes</span>
