5954c59d6a05138a8c0793382a76a77fdc4bfce5
Eliot Austin-Forbes <ejaustinforbes@gmail.com>
2023-02-25 18:36:07 +0900

    chore: remove header no ssr

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -6,29 +6,23 @@ import { usePathname } from 'next/navigation'
 import { useRouter } from 'next/navigation'
 
 import logo from '@/components/logo'
-import withoutSsr from '@/components/noSsrHoc'
 import { useAuth } from '@/contexts/authContext'
 import { auth } from '@/firebase/clientApp'
 
 import { Slogan, StyledHeader, StyledNavbar } from './styles'
 
-function Header() {
+export default function Header() {
   const isAuthenticated = useAuth()
   const path = usePathname()
   const router = useRouter()
   const isRoot = path == '/'
   const isDashboard = path == '/dashboard'
   const isNavigable = path !== '/reg'
-  const isSessionPendingLogin = !!sessionStorage.getItem('snai:pendingLogin')
 
   return (
     <StyledHeader isRoot={isRoot}>
       <div>
-        {isRoot || isSessionPendingLogin ? (
-          <div>{logo}</div>
-        ) : (
-          <Link href="/">{logo}</Link>
-        )}
+        {isRoot ? <div>{logo}</div> : <Link href="/">{logo}</Link>}
         {isNavigable && (
           <StyledNavbar>
             {isAuthenticated !== null &&
@@ -57,5 +51,3 @@ function Header() {
     </StyledHeader>
   )
 }
-
-export default withoutSsr(Header)
