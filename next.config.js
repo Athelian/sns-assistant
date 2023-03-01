5bd49e5e963cc19d37b886300c6e2b31360dcb1a
Eliot Austin-Forbes <ejaustinforbes@gmail.com>
2023-03-01 16:33:48 +0900

    chore: remove redirects

--- next.config.js
+++ next.config.js
@@ -8,34 +8,6 @@ const nextConfig = {
   publicRuntimeConfig: {
     NODE_ENV: env.NODE_ENV,
   },
-  async redirects() {
-    return [
-      {
-        source: '/reg',
-        has: [
-          {
-            type: 'cookie',
-            key: 'authorized',
-            value: 'true',
-          },
-        ],
-        permanent: false,
-        destination: '/dashboard',
-      },
-      {
-        source: '/dashboard',
-        has: [
-          {
-            type: 'cookie',
-            key: 'authorized',
-            value: 'false',
-          },
-        ],
-        permanent: false,
-        destination: '/reg',
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
