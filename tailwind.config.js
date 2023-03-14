d2e00dc1e49c9e07810e5ace2bb92372806b787a
Eliot Austin-Forbes <ejaustinforbes@gmail.com>
2023-03-14 22:57:15 +0900

    fix: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './app/**/*.{ts,tsx}', // Note the addition of the `app` directory.
-    './components/**/*.{ts,tsx}',
-  ],
+  content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
   theme: {
     extend: {},
   },
