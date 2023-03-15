81ea3a269f6d68134f2a35ccb0f219c8eab6911c
Eliot Austin-Forbes <ejaustinforbes@gmail.com>
2023-03-15 20:41:32 +0900

    feat: provider links

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,7 +1,17 @@
+import { faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { type NextPage } from 'next'
+import Link from 'next/link'
 
 const Dashboard: NextPage = () => {
-  return <></>
+  return (
+    <div className="flex justify-center gap-64">
+      <Link href="/facebook">
+        <FontAwesomeIcon color="#3B5998" icon={faFacebook} size="10x" />
+      </Link>
+      <FontAwesomeIcon color="gray" icon={faTwitter} size="10x" />
+    </div>
+  )
 }
 
 export default Dashboard
