2f7f5b05cfa8cdace27cdbaac73418889c7e31b8
Eliot Austin-Forbes <ejaustinforbes@gmail.com>
2023-03-21 22:54:33 +0900

    feat: provider css

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -5,12 +5,20 @@ import Link from 'next/link'
 
 const Dashboard: NextPage = () => {
   return (
-    <div className="flex justify-center gap-64">
-      <Link href="/facebook">
-        <FontAwesomeIcon color="#3B5998" icon={faFacebook} size="10x" />
-      </Link>
-      <FontAwesomeIcon color="gray" icon={faTwitter} size="10x" />
-    </div>
+    <section className="flex justify-center">
+      <div className="flex gap-8 w-full justify-around px-7 max-w-[980px]">
+        <Link className="w-1/3 min-w-[260px]" href="/facebook">
+          <FontAwesomeIcon
+            className="text-[#3B5998] hover:text-gray-500"
+            icon={faFacebook}
+          />
+        </Link>
+        <FontAwesomeIcon
+          className="w-1/3 min-w-[260px] text-gray-500"
+          icon={faTwitter}
+        />
+      </div>
+    </section>
   )
 }
 
